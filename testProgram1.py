c0c8c3abb3b84ab2bcf3e74b7b566be9c62fe03d
Adam-Grimes <05adamg@gmail.com>
2025-10-15 13:25:16 +0100

    new programs for unit testing

---
+++ testProgram1.py
@@ -0,0 +1,9 @@
+choice = input("Enter x or y")
+
+if (choice == "x"):
+    print("You chose x")
+ 
+elif (choice == "y"):
+    print("You chose y")
+else:
+    print("You didn't enter a correct letter (x/y)")
---
+++ testProgram2.py
@@ -0,0 +1,9 @@
+choice = int(input("Enter a number"))
+
+if (choice < 20 or choice ==20):
+    print("your answer was less than or equal to 20")
+ 
+elif (choice > 20):
+    print("your answer was greater than 30")
+else:
+    print("you did not enter a correct input")
